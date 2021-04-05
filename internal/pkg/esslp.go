3febcb11991f5100bbf2e2be1a5faebfb5018a55
owen <owen@howlandedgerton.com>
2021-04-05 05:03:59 +0000

    bounds testing and simple lp solver

--- internal/pkg/esslp.go
+++ internal/pkg/esslp.go
@@ -2,8 +2,10 @@ package esslp
 
 import (
 	"errors"
+	"math"
 
 	"github.com/google/uuid"
+	"github.com/lanl/clp"
 )
 
 type Unit struct {
@@ -13,13 +15,14 @@ type Unit struct {
 	Cc          float64 // cost of energy capacity
 	Ce          float64 // cost of energy storage
 	constraints [][]float64
+	bounds      [][]float64
 }
 
 func NewUnit(pid uuid.UUID, Cp float64, Cn float64, Cc float64, Ce float64) Unit {
-	return Unit{pid, Cp, Cn, Cc, Ce, [][]float64{}}
+	return Unit{pid, Cp, Cn, Cc, Ce, [][]float64{}, [][]float64{}}
 }
 
-func (u Unit) Flatten() []float64 {
+func (u Unit) CostCoefficients() []float64 {
 	return []float64{u.Cp, u.Cn, u.Cc, u.Ce}
 }
 
@@ -39,21 +42,33 @@ func (u Unit) Constraints() [][]float64 {
 	return u.constraints
 }
 
+func (u Unit) Bounds() [][2]float64 {
+	inf := math.Inf(1)
+	return [][2]float64{
+		{0, inf},
+		{0, inf},
+		{0, inf},
+		{0, inf},
+	}
+}
+
 type Group struct {
-	Units []Unit
+	units       []Unit
+	constraints [][]float64
 }
 
 func NewGroup(units ...Unit) Group {
 	ux := make([]Unit, 0)
 	ux = append(ux, units...)
+	cx := make([][]float64, 0)
 
-	return Group{ux}
+	return Group{ux, cx}
 }
 
 func (g Group) CostCoefficients() []float64 {
 	cx := make([]float64, 0)
-	for _, u := range g.Units {
-		cx = append(cx, u.Flatten()...)
+	for _, u := range g.units {
+		cx = append(cx, u.CostCoefficients()...)
 	}
 
 	return cx
@@ -61,9 +76,53 @@ func (g Group) CostCoefficients() []float64 {
 
 func (g Group) ColumnSize() int {
 	var s int
-	for _, u := range g.Units {
+	for _, u := range g.units {
 		s += u.ColumnSize()
 	}
 
 	return s
+}
+
+func (g *Group) NewConstraint(c []float64) error {
+	if len(c) != g.ColumnSize() {
+		return errors.New("column size mismatch")
+	}
+	g.constraints = append(g.constraints, c)
+	return nil
+}
+
+func (g Group) Constraints() [][]float64 {
+	return g.constraints
+}
+
+func (g Group) Bounds() [][2]float64 {
+	b := make([][2]float64, 0)
+
+	for _, u := range g.units {
+		b = append(b, u.Bounds()...)
+	}
+	return b
+}
+
+// Linear Program
+
+type LinearProgram struct {
+	group *Group
+}
+
+func NewLP(g *Group) LinearProgram {
+	return LinearProgram{g}
+}
+
+func (lp *LinearProgram) Solve() []float64 {
+	s := clp.NewSimplex()
+	s.EasyLoadDenseProblem(
+		lp.group.CostCoefficients(),
+		lp.group.Bounds(),
+		lp.group.Constraints(),
+	)
+
+	s.SetOptimizationDirection(clp.Minimize)
+	s.Primal(clp.NoValuesPass, clp.NoStartFinishOptions)
+	return s.PrimalColumnSolution()
 }
--- internal/pkg/esslp_test.go
+++ internal/pkg/esslp_test.go
@@ -22,7 +22,7 @@ func TestNewAssetVar(t *testing.T) {
 	assert.Equal(t, a.Ce, Ce)
 }
 
-func TestFlattenAssetVar(t *testing.T) {
+func TestGetCostCoefficients(t *testing.T) {
 	pid, _ := uuid.NewUUID()
 	Cp := 1.0
 	Cn := 2.0
@@ -31,7 +31,7 @@ func TestFlattenAssetVar(t *testing.T) {
 
 	a := NewUnit(pid, Cp, Cn, Cc, Ce)
 
-	assert.Equal(t, a.Flatten(), []float64{1.0, 2.0, 3.0, 4.0})
+	assert.Equal(t, a.CostCoefficients(), []float64{1.0, 2.0, 3.0, 4.0})
 }
 
 func TestGetUnitColumnSize(t *testing.T) {
@@ -52,20 +52,69 @@ func TestApplyUnitConstraint(t *testing.T) {
 
 }
 
+func TestBadConstraint(t *testing.T) {
+	pid1, _ := uuid.NewUUID()
+	a1 := NewUnit(pid1, 1.0, 2.0, 3.0, 4.0)
+	c := []float64{2.0, 0, 1.0, 0.3, 0.1}
+
+	err := a1.NewConstraint(c)
+	assert.Error(t, err)
+}
+
+func TestReturnConstraints(t *testing.T) {
+	pid1, _ := uuid.NewUUID()
+	a1 := NewUnit(pid1, 1.0, 2.0, 3.0, 4.0)
+	c1 := []float64{0.0, 1.0, 2.0, 3.0}
+	c2 := []float64{1.1, 2.2, 3.3, 4.4}
+	c3 := []float64{-0.3, 0.2, -0.1, 0.0}
+
+	err := a1.NewConstraint(c1)
+	assert.Nil(t, err)
+	err = a1.NewConstraint(c2)
+	assert.Nil(t, err)
+	err = a1.NewConstraint(c3)
+	assert.Nil(t, err)
+
+	cx := a1.Constraints()
+
+	for i, c := range cx {
+		switch i {
+		case 0:
+			assert.Equal(t, c, c1)
+		case 1:
+			assert.Equal(t, c, c2)
+		case 2:
+			assert.Equal(t, c, c3)
+		default:
+			assert.Fail(t, "unexpected constraint in unit: %v\n", c)
+		}
+	}
+}
+
+func TestUnitBounds(t *testing.T) {
+	pid1, _ := uuid.NewUUID()
+	a1 := NewUnit(pid1, 1.0, 2.0, 3.0, 4.0)
+
+	b := a1.Bounds()
+	assert.Fail(t, "bounds not specified for test %v", b)
+}
+
+// GROUP TESTS
+
 func TestNewAssetVarsGroup(t *testing.T) {
 
 	ag0 := NewGroup()
-	assert.Equal(t, ag0.Units, []Unit{}, "empty group does not return empty units slice")
+	assert.Equal(t, ag0.units, []Unit{}, "empty group does not return empty units slice")
 
 	pid1, _ := uuid.NewUUID()
 	a1 := NewUnit(pid1, 1.0, 2.0, 3.0, 4.0)
 	ag1 := NewGroup(a1)
-	assert.Equal(t, ag1.Units, []Unit{a1}, "group does not contain unit assigned in new")
+	assert.Equal(t, ag1.units, []Unit{a1}, "group does not contain unit assigned in new")
 
 	pid2, _ := uuid.NewUUID()
 	a2 := NewUnit(pid2, 5.0, 6.0, 7.0, 8.0)
 	ag2 := NewGroup(a1, a2)
-	assert.Equal(t, ag2.Units, []Unit{a1, a2}, "group does not contain multiple units assigned in new")
+	assert.Equal(t, ag2.units, []Unit{a1, a2}, "group does not contain multiple units assigned in new")
 }
 
 func TestGetGroupDecisionVariableCoefficients(t *testing.T) {
@@ -91,4 +140,57 @@ func TestGetGroupColumnsSize(t *testing.T) {
 	ag2 := NewGroup(a1, a2)
 
 	assert.Equal(t, ag2.ColumnSize(), 8)
+}
+
+func TestNewGroupConstraint(t *testing.T) {
+	pid1, _ := uuid.NewUUID()
+	pid2, _ := uuid.NewUUID()
+	a1 := NewUnit(pid1, 1.0, 2.0, 3.0, 4.0)
+	a2 := NewUnit(pid2, 5.0, 6.0, 7.0, 8.0)
+	ag1 := NewGroup(a1, a2)
+
+	c := []float64{-1.0, 0, 1.0, 0, 1.0, 0.2, 2.1, 1.1}
+
+	err := ag1.NewConstraint(c)
+	assert.Nil(t, err)
+	assert.Equal(t, ag1.Constraints()[0], c)
+}
+
+func TestBadGroupConstraint(t *testing.T) {
+	pid1, _ := uuid.NewUUID()
+	pid2, _ := uuid.NewUUID()
+	a1 := NewUnit(pid1, 1.0, 2.0, 3.0, 4.0)
+	a2 := NewUnit(pid2, 5.0, 6.0, 7.0, 8.0)
+	ag1 := NewGroup(a1, a2)
+
+	c := []float64{-1.0, 1.1}
+
+	err := ag1.NewConstraint(c)
+	assert.Error(t, err)
+}
+
+func TestGroupBounds(t *testing.T) {
+	pid1, _ := uuid.NewUUID()
+	pid2, _ := uuid.NewUUID()
+	a1 := NewUnit(pid1, 1.0, 2.0, 3.0, 4.0)
+	a2 := NewUnit(pid2, 5.0, 6.0, 7.0, 8.0)
+	ag1 := NewGroup(a1, a2)
+
+	bounds := ag1.Bounds()
+	assert.Fail(t, "bounds %v", bounds)
+}
+
+// LP
+
+func TestSimpleLPFormulation(t *testing.T) {
+	pid1, _ := uuid.NewUUID()
+	pid2, _ := uuid.NewUUID()
+	a1 := NewUnit(pid1, 1.0, 2.0, 3.0, 4.0)
+	a2 := NewUnit(pid2, 5.0, 6.0, 7.0, 8.0)
+	ag1 := NewGroup(a1, a2)
+
+	lp := NewLP(&ag1)
+
+	sol := lp.Solve()
+	assert.Fail(t, "sol[0] = %v", sol[0])
 }
